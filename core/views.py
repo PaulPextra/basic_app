3ba382f4df10ee28b56ebaee66173676627532d7
Paul Okoli <paulokoli7@gmail.com>
2022-06-02 19:24:51 +0100

    Basic Django App

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView, CreateView, UpdateView
+from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
